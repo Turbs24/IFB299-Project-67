3cedb4b7cc3a93deb9af04e660ec4e534ac9de02
Turbs24 <41731072+Turbs24@users.noreply.github.com>
2018-10-04 19:03:32 +1000

    del repo

--- IFB299/homepage/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.url import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.home),
-    ]
--- crcapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Car
-
-@admin.register(Car)
-class CarAdmin(admin.ModelAdmin):
-	list_display=['Car_MakeName','Car_Model','Car_Series','Car_SeriesYear','Car_PriceNew']
